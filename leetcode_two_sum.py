cded3affb22f3bff196ab3601f44be391fc5b718
K Ramkrishna Achary <kramkrishnaachary@Rams-MacBook-Pro.local>
2025-02-08 22:52:41 +0530

    added script for twoSum problem

---
+++ leetcode_two_sum.py
@@ -0,0 +1,66 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+"""
+
+class Solution:
+    # Approach: Brute Force
+    def twoSum(self, nums: list[int], target: int):
+        try:
+            for index in range(len(nums)):
+                for index2 in range(index + 1, len(nums)):
+                    if nums[index] + nums[index2] == target and index != index2:
+                        return [index, index2]
+        except Exception as error:
+            print(f" Exception occurred: {error}")
+        return []
+    
+    # Approach: Sorting and Two Pointers
+    def twoSum2(self, nums: list[int], target: int):
+    
+        indexed_nums = [(val, idx) for idx, val in enumerate(nums)] # Store values along with their original indices
+        indexed_nums.sort() # Sort based on values
+
+        index1 = 0
+        index2 = len(indexed_nums) - 1
+
+        while index1 < index2:
+            sum_value = indexed_nums[index1][0] + indexed_nums[index2][0]
+            if sum_value == target:
+                return [indexed_nums[index1][1], indexed_nums[index2][1]]
+            elif sum_value < target:
+                index1 += 1
+            else:
+                index2 -= 1
+        return []
+    
+s = Solution()
+nums = [2, 11, 15 ,7]
+target = 9
+
+
+index_list = s.twoSum(nums, target)
+if len(index_list) == 0:
+    print(f"Input array: {nums}")
+    print(f"Input target: {target}")
+    print("No two numbers add up to the target.")
+else:
+    print(f"Input array: {nums}")
+    print(f"Input target: {target}")
+    print(f"Indices of the two numbers that add up to the target: {index_list}")
+
+index_list = s.twoSum2(nums, target)
+if len(index_list) == 0:
+    print(f"Input array: {nums}")
+    print(f"Input target: {target}")
+    print("No two numbers add up to the target.")
+else:
+    print(f"Input array: {nums}")
+    print(f"Input target: {target}")
+    print(f"Indices of the two numbers that add up to the target: {index_list}")
--- maximum_number.py
+++ maximum_number.py
@@ -10,12 +10,12 @@ range_end = 10000
 random_numbers = random.choices(range(range_start, range_end + 1), k=num_numbers)
 
 # Find the maximum number
-def find_max_number(numbers): # Define a function that takes a list of numbers as input
-    max_number = numbers[0] # Initialize max_number to the first number
-    for number in numbers: # Iterate through the list of numbers
-        if number > max_number: # If the current number is greater than max_number
-            max_number = number  # Update max_number to the current number
-    return max_number # Return the maximum number
+def find_max_number(numbers):
+    max_number = numbers[0]
+    for number in numbers:
+        if number > max_number:
+            max_number = number
+    return max_number
 
 # Measure time taken to check for duplicates
 start_time = time.time()
@@ -23,5 +23,5 @@ max_number = find_max_number(random_numbers)
 end_time = time.time()
 total_time = end_time - start_time
 
-print(f"Maximum number: {max_number}") # Print the maximum number
-print(f"Time taken: {total_time} seconds")  # Print the time taken to find the maximum number
+print(f"Maximum number: {max_number}")
+print(f"Time taken: {total_time} seconds")
