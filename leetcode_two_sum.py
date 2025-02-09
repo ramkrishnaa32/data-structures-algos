42f04cbcd6ab22ad97e87e1ff8c70e47ad3dc4de
K Ramkrishna Achary <kramkrishnaachary@Rams-MacBook-Pro.local>
2025-02-09 22:24:48 +0530

    added script for twoSum problem

--- leetcode_two_sum.py
+++ leetcode_two_sum.py
@@ -33,16 +33,27 @@ class Solution:
         while index1 < index2:
             sum_value = indexed_nums[index1][0] + indexed_nums[index2][0]
             if sum_value == target:
-                return [indexed_nums[index1][1], indexed_nums[index2][1]]
+                return [indexed_nums[index1][1], indexed_nums[index2][1]] 
             elif sum_value < target:
-                index1 += 1
+                index1 += 1 
             else:
                 index2 -= 1
         return []
     
+    # Approach: Hash Table
+    def twoSum3(self, nums: list[int], target: int):
+        nums_dict = {}
+        for index, value in enumerate(nums):
+            difference = target - value
+            if difference in nums_dict:
+                return [nums_dict[difference], index]
+            nums_dict[value] = index
+        return []
+   
+    
 s = Solution()
-nums = [2, 11, 15 ,7]
-target = 9
+nums = [3, 2, 5, 3, 10]
+target = 6
 
 
 index_list = s.twoSum(nums, target)
@@ -56,6 +67,16 @@ else:
     print(f"Indices of the two numbers that add up to the target: {index_list}")
 
 index_list = s.twoSum2(nums, target)
+if len(index_list) == 0:
+    print(f"Input array: {nums}")
+    print(f"Input target: {target}")
+    print("No two numbers add up to the target.")
+else:
+    print(f"Input array: {nums}")
+    print(f"Input target: {target}")
+    print(f"Indices of the two numbers that add up to the target: {index_list}")
+
+index_list = s.twoSum3(nums, target)
 if len(index_list) == 0:
     print(f"Input array: {nums}")
     print(f"Input target: {target}")
