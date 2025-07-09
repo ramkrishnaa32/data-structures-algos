67488f9b067bf2d1ca3e8283429c4d65d6ee48e5
K Ramkrishna Achary <krachary@myastrotech.com>
2025-07-09 17:59:11 +0530

    updated the script fof leadersInArray

--- python/leadersInArray.py
+++ python/leadersInArray.py
@@ -2,15 +2,14 @@ class Solution:
     def leadersInArray(self, nums):
         maxNum = 0
         answer = []
-        for i in range(len(nums)-1,-1,-1 ):
-            if  nums[i] > maxNum:
+        for i in range(len(nums)-1, -1, -1):
+            if maxNum < nums[i]:
                 maxNum = max(maxNum, nums[i])
                 answer.append(nums[i])
-                print(f"num: {nums[i]}, maxNum: {maxNum}, answer: {answer}")
+                print(f"index: {i}, num: {nums[i]}, maxNum: {maxNum}, answer: {answer}")
         return answer
 
-
-nums = [16, 17, 4, 3, 5, 2]
+nums = [16, 17, 4, 3, 5, 2, 3]
 s = Solution()
 result = s.leadersInArray(nums)
 print(result)
