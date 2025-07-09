463cd24369afce6a84ff0d503e7081b2ecb2460a
K Ramkrishna Achary <krachary@myastrotech.com>
2025-07-09 16:10:46 +0530

    updated maxArea script

--- python/water_container.py
+++ python/water_container.py
@@ -1,27 +1,24 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        maxWater = 0
+        while left < right:
+            w = right - left
+            h = min(height[left], height[right])
+            currentWater = w * h
+            maxWater = max(maxWater, currentWater)
+            print(f"sub-array:", height[left:right + 1])
+            print(f"width: {w}, height: {h}, currentWater: {currentWater}, maxArea: {maxWater}")
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
 
-def maxArea(height):
-    """
-    :type height: List[int]
-    :rtype: int
-    """
-    left = 0
-    right = len(height) - 1
-    maxWater = 0
-    while left < right:
-        w = right - left
-        h = min(height[left], height[right])
-        currentWater = w * h
-        maxWater = max(maxWater, currentWater)
-        print(f"left: {left}, right: {right}")
-        print(f"w: {w}, h: {h}, currentWater: {currentWater}, maxWater: {maxWater}")
-        if height[left] < height[right]:
-            left += 1
-        else:
-            right -= 1
-    return maxWater
+        return maxWater
 
-nums = [8, 7, 2, 1]
-result = maxArea(nums)
-print(f"maxWater: {result}")
+
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+s = Solution()
+result = s.maxArea(height)
+print(f"result: {result}")
