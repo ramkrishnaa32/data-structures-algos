04cfee070677984226995c65adf051e895ac5263
K Ramkrishna Achary <krachary@myastrotech.com>
2025-07-09 16:19:20 +0530

    updated the bruteforce approach for maxarea

--- python/water_container.py
+++ python/water_container.py
@@ -1,4 +1,20 @@
+from typing import List
+
+
 class Solution:
+
+    def maxAreaBruteForce(self, height: list[int]) -> int:
+        maxWater = 0
+        for index1 in range(len(height)):
+            for index2 in range(index1+1, len(height)):
+                w = index2 - index1
+                h = min(height[index1], height[index2])
+                currentWater = w * h
+                maxWater = max(currentWater, maxWater)
+
+        return maxWater
+
+
     def maxArea(self, height: list[int]) -> int:
         left = 0
         right = len(height) - 1
@@ -21,4 +37,7 @@ class Solution:
 height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
 s = Solution()
 result = s.maxArea(height)
+print(f"result: {result}")
+
+result = s.maxAreaBruteForce(height)
 print(f"result: {result}")
