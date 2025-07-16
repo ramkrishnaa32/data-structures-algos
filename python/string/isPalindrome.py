0289c8367c0d3cfd254635f1509602611672a08d
K Ramkrishna Achary <krachary@myastrotech.com>
2025-07-16 12:34:00 +0530

    updated the script for isPalindrome

---
+++ python/string/isPalindrome.py
@@ -0,0 +1,44 @@
+"""
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+"""
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        start = 0
+        end = len(s) - 1
+        while start < end:
+            if not s[start].isalnum():
+                start += 1
+                continue
+            if not s[end].isalnum():
+                end -= 1
+                continue
+
+            if s[start].lower() != s[end].lower():
+                return False
+
+            start += 1
+            end -= 1
+
+        return True
+
+
+# Test
+s = "A man, a plan, a canal: Panama"
+# s = "race a car"
+sol = Solution()
+result = sol.isPalindrome(s)
+print(result)  # True for first string
